908febd5aef9e9418fa7f54166130e8fd651cc13
Vel <vel.nat@gmail.com>
2018-06-04 13:09:58 -0500

    Create headers.js

---
+++ src/test/java/config/headers.js
@@ -0,0 +1,13 @@
+unction() {
+    var headers = {
+    'Accept': 'application/json',
+    'Client-id': 'xyz45463',
+    'authorization': 'Bearer zzzzyyyy',
+    'sourcesystemcode': 'A96',
+    'sourcetrackingid': '1234',
+    'sourcesytemrequesttimestamp': '2016-09-13T23:30:52.123Z',
+    'assetnumber': 'W071',
+    'userid': 'X12343216'    
+     }
+  return headers;
+}
